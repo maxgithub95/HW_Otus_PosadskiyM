cff660ee9f3d371522fd0514f0938c8846852eb5
DESKTOP-48PS9TT\Макс <maxfire.95@mail.ru>
2022-11-10 23:49:50 +0300

    Выполнил первые три пункта ДЗ

--- HW_8/HW_8/ProgramHW_8.cs
+++ HW_8/HW_8/ProgramHW_8.cs
@@ -2,24 +2,69 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public class Node
         {
-            public class TreeOfSalary
+            public string Name { get; set; }
+            public int Salary { get; set; }
+            public Node LeftDaughter { get; set; }
+            public Node RightDaughter { get; set; }
+
+        }
+
+        static void Main()
+        {
+            static void AddNode(Node root, Node getNode)
             {
-            string Name { get; set; }
-            int Salary { get; set; }
-
-
-            }
-
-            byte i = 1;
-            while (Console.ReadLine() != "/n")
-            {
-                if (i == 1) 
+                if (getNode.Salary < root.Salary)
                 {
-                    Console.WriteLine("Hello");
+                    if (root.LeftDaughter == null) root.LeftDaughter = getNode;
+                    else AddNode(root.LeftDaughter, getNode);
+                }
+                else
+                {
+                    if (root.RightDaughter == null) root.RightDaughter = getNode;
+                    else AddNode(root.RightDaughter, getNode);
+                }
+             }
+            static void GetAscendingList(Node startRoot)
+            {
+                if (startRoot.LeftDaughter != null)
+                {
+                    GetAscendingList(startRoot.LeftDaughter);
+                }
+                Console.Write(startRoot.Name + " ");
+                Console.WriteLine(startRoot.Salary);
+                if (startRoot.RightDaughter != null)
+                {
+                    GetAscendingList(startRoot.RightDaughter);
                 }
             }
+            Node root = null;
+            while (true)
+            {
+                Console.WriteLine("Enter name:");
+                string name = Console.ReadLine();
+                if (name == "") break;
+                Console.WriteLine("Enter salary:");
+                int salary = Int32.Parse(Console.ReadLine());
+
+                Node nodeToAdd = new Node
+                {
+                    Name = name,
+                    Salary = salary
+                };
+                if (root == null)
+                {
+                    root = nodeToAdd;
+                }
+                else
+                {
+                    AddNode(root, nodeToAdd);
+                }
+            }
+            if (root == null) Console.WriteLine("Tree is empty");
+            else GetAscendingList(root);
+
         }
     }
 }
