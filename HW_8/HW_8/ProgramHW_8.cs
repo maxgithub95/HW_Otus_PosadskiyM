4ae2b902933c97bc6570b7c8ba911d89bc17d7da
DESKTOP-48PS9TT\Макс <maxfire.95@mail.ru>
2022-11-10 21:22:51 +0300

    пробую настроить Gitignore

--- HW_8/HW_8/ProgramHW_8.cs
+++ HW_8/HW_8/ProgramHW_8.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            
+            public class TreeOfSalary
+            {
+            string Name { get; set; }
+            int Salary { get; set; }
+
+
+            }
+
+            byte i = 1;
+            while (Console.ReadLine() != "/n")
+            {
+                if (i == 1) 
+                {
+                    Console.WriteLine("Hello");
+                }
+            }
         }
     }
 }
