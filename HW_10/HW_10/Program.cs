13ea55924cc969816f3b6194079cd3c3ab23633a
DESKTOP-48PS9TT\Макс <maxfire.95@mail.ru>
2022-12-11 00:41:08 +0300

    Продолжаю выполнять дз

--- HW_10/HW_10/Program.cs
+++ HW_10/HW_10/Program.cs
@@ -1,9 +1,10 @@
-﻿using static System.IO.Path;
+﻿using System.Xml.Linq;
+using static System.IO.Path;
 namespace HW_10
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             string dir1 = Combine("D:", "Otus", "TestDir1");
             string dir2 = Combine("D:", "Otus", "TestDir2");
@@ -12,22 +13,54 @@ namespace HW_10
             byte Amount = 10;
             byte k=0;
             if (!File.Exists(Combine(dir2, $"File{Amount}.txt")))
-            {
+              {
                 while (k != Amount)
                 {
-
-                    File.CreateText(Combine(dir1, $"File{k + 1}.txt"));
-                    File.CreateText(Combine(dir2, $"File{k + 1}.txt"));
+                    string name = $"File{k + 1}.txt";
+                    try
+                    {
+                    using (var textWriter = File.CreateText(Combine(dir1, name)))
+                    {
+                        textWriter.WriteLine(name);
+                    }
+                    using (var textWriter = File.CreateText(Combine(dir2, name)))
+                    {
+                        textWriter.WriteLine(name);
+                    }
+                    }
+                    catch(Exception e) { Console.WriteLine(e.Message); }
                     k++;
                 }
-            }
+              }
             else Console.WriteLine("Файлы уже созданы");
-            var NameOfFiles = Directory.GetFileSystemEntries(dir1);
-            foreach (string name in NameOfFiles)
+
+            
+            var Files = Directory.GetFileSystemEntries(dir1).Concat(Directory.GetFileSystemEntries(dir2));
+            foreach (string file in Files)
             {
-                Console.WriteLine(name);
+                 WriteData(file);
+             Console.WriteLine(ReadData(file));
             }
+      
            
+        }
+
+        private static bool ReadData(string file)
+        {
+            throw new NotImplementedException();
+        }
+
+        static void WriteData(string path)
+        {
+            try 
+            { 
+            using (StreamWriter textWriter = new StreamWriter(path, true))
+                {
+                   textWriter.WriteLine(DateTime.Now);
+                }
+            }
+            catch(Exception e) { Console.WriteLine(e.Message); }
+
         }
     }
 }
