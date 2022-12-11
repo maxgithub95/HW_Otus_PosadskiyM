488b8d6e63e405cddce334e3ad1474f9f6924861
DESKTOP-48PS9TT\Макс <maxfire.95@mail.ru>
2022-12-11 20:21:55 +0300

    Выполнил ДЗ10

--- HW_10/HW_10/Program.cs
+++ HW_10/HW_10/Program.cs
@@ -1,4 +1,4 @@
-﻿using System.Xml.Linq;
+﻿using System.Text;
 using static System.IO.Path;
 namespace HW_10
 {
@@ -11,55 +11,72 @@ namespace HW_10
             Directory.CreateDirectory(dir1);
             Directory.CreateDirectory(dir2);
             byte Amount = 10;
-            byte k=0;
+            byte k = 0;
             if (!File.Exists(Combine(dir2, $"File{Amount}.txt")))
-              {
+            {
                 while (k != Amount)
                 {
                     string name = $"File{k + 1}.txt";
                     try
                     {
-                    using (var textWriter = File.CreateText(Combine(dir1, name)))
-                    {
-                        textWriter.WriteLine(name);
+                        using (var textWriter = File.CreateText(Combine(dir1, name)))
+                        {
+                            textWriter.WriteLine(name);
+                        }
+                        using (var textWriter = File.CreateText(Combine(dir2, name)))
+                        {
+                            textWriter.WriteLine(name);
+                        }
                     }
-                    using (var textWriter = File.CreateText(Combine(dir2, name)))
-                    {
-                        textWriter.WriteLine(name);
-                    }
-                    }
-                    catch(Exception e) { Console.WriteLine(e.Message); }
+                    catch (Exception e) { Console.WriteLine(e.Message); }
                     k++;
-                }
-              }
-            else Console.WriteLine("Файлы уже созданы");
 
-            
+                }
+                var files = Directory.GetFileSystemEntries(dir1).Concat(Directory.GetFileSystemEntries(dir2));
+                foreach (string file in files)
+                {
+                    WriteData(file);
+                }
+
+            }
+            else Console.WriteLine("Файлы уже созданы");
             var Files = Directory.GetFileSystemEntries(dir1).Concat(Directory.GetFileSystemEntries(dir2));
             foreach (string file in Files)
             {
-                 WriteData(file);
-             Console.WriteLine(ReadData(file));
+                Console.WriteLine(ReadFiles(file));
             }
-      
-           
+
         }
 
-        private static bool ReadData(string file)
+        static StringBuilder ReadFiles(string path)
         {
-            throw new NotImplementedException();
+            StringBuilder result = new StringBuilder();
+            try
+            {
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    string[] Path = path.Split('\\');
+                    string name = Path[Path.Length - 1];
+                    string text = reader.ReadLine();
+                    string data = reader.ReadLine();
+                    result.Append($"{name}: {text} + {data}.");
+                }
+            }
+            catch (Exception e) { Console.WriteLine(e.Message); }
+            return result;
+
         }
 
         static void WriteData(string path)
         {
-            try 
-            { 
-            using (StreamWriter textWriter = new StreamWriter(path, true))
+            try
+            {
+                using (StreamWriter textWriter = new StreamWriter(path, true))
                 {
-                   textWriter.WriteLine(DateTime.Now);
+                    textWriter.WriteLine(DateTime.Now);
                 }
             }
-            catch(Exception e) { Console.WriteLine(e.Message); }
+            catch (Exception e) { Console.WriteLine(e.Message); }
 
         }
     }
