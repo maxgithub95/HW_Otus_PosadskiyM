a98d8b1256981312decd33f5faa468b9e890a7a4
DESKTOP-48PS9TT\Макс <maxfire.95@mail.ru>
2022-12-12 22:03:52 +0300

    Сделал класс ImageDownloader, реализовал скачивание картинки.

---
+++ HW_9/HW_9/HW_9/ImageDownloader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_9
+{
+    internal class ImageDownloader
+    {
+        
+        string remoteUri{get;set;}
+        string fileName { get; set; }
+
+        WebClient myWebClient;
+        public ImageDownloader(string _remoteUri, string _fileName)
+        {   
+            myWebClient = new WebClient();
+            remoteUri = _remoteUri;
+            fileName = _fileName;
+            
+        }
+
+        public void Download()
+        {
+            Console.WriteLine("Качаю \"{0}\" из \"{1}\" .......\n\n", fileName, remoteUri);
+            myWebClient.DownloadFile(remoteUri, fileName);
+            Console.WriteLine("Успешно скачал \"{0}\" из \"{1}\"", fileName, remoteUri);
+        }
+       
+    }
+}
+// = "https://zastavok.net/download/61117/1920x1200/";
+//= "bigimage.jpg";
--- HW_9/HW_9/HW_9/Program.cs
+++ HW_9/HW_9/HW_9/Program.cs
@@ -7,7 +7,12 @@ namespace HW_9
 
         static void Main(string[] args)
         {
-            
+            string picutreUri = "https://zastavok.net/download/61117/1920x1200/";
+            string Name = "bigimage.jpg";
+            var imageDownload = new ImageDownloader(picutreUri, Name);
+            imageDownload.Download();
+            Console.WriteLine("Нажмите любую клавишу для выхода");
+            Console.ReadKey();
         }
     }
 }
