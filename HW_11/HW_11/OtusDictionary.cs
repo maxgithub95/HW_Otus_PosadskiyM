e2b7ddc51c0b48063bdf30eaf2b78960e3f2e606
DESKTOP-48PS9TT\Макс <maxfire.95@mail.ru>
2022-12-20 20:19:45 +0300

    Начал выполнять ДЗ11

---
+++ HW_11/HW_11/OtusDictionary.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_11
+{
+    public class OtusDictionary
+    {
+        public Elementes[] element = new Elementes[32];
+        public void Add(int key, string value)
+        {
+            if (value == null) throw new ArgumentNullException("value");
+            int hash = key % 32;
+            if (element[hash] == null) { element[hash]= new Elementes(key, value); }
+            else { throw new ElementFilledException("место занято"); }
+
+        }
+        public string Get(int key)
+        {
+            return "0";
+        }
+    }
+
+    [Serializable]
+    internal class ElementFilledException : Exception
+    {
+        public ElementFilledException()
+        {
+        }
+
+        public ElementFilledException(string? message) : base(message)
+        {
+        }
+
+        public ElementFilledException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected ElementFilledException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+
+    public class Elementes
+    {
+        public Elementes(int key, string value)
+        {
+            Key = key;
+            Value = value;
+        }
+
+        public int Key { get; set; }
+        public string Value { get; set; }
+    }
+}
---
+++ HW_11/HW_11/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HW_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
