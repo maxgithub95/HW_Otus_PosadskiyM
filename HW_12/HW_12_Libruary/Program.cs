83e66789569031b93c7ca1ac3d1021027db49c53
DESKTOP-48PS9TT\Макс <maxfire.95@mail.ru>
2022-12-28 18:49:34 +0300

    Сделана задача "Библиотека" в дз 12

--- HW_12/HW_12_Libruary/Program.cs
+++ HW_12/HW_12_Libruary/Program.cs
@@ -1,10 +1,60 @@
-﻿namespace HW_12_Libruary
+﻿using System.Collections.Concurrent;
+
+namespace HW_12_Libruary
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ConcurrentDictionary<string, int> Library = new ConcurrentDictionary<string, int>();
+            Task.Run(async () =>
+            {
+                while (true)
+                {
+                    if (Library.Count > 0)
+                    {
+                        foreach (var book in Library)
+                        {
+                            if (book.Value < 100) Library[book.Key]++;
+                            else Library.TryRemove(book);
+                        }
+                    }
+                    await Task.Delay(1000);
+                }
+            });
+            ConsoleKeyInfo key;
+            do
+            {
+                Console.WriteLine("Меню:\n1 - добавить книгу;\n2 - вывести список непрочитанного;\n3 - отчистить экран ;\n4 - выйти.\n");
+                key = Console.ReadKey(true);
+                switch (key.Key)
+                {
+                    case ConsoleKey.D1:
+                        Console.WriteLine("Введите название книги:");
+                        string NameOfBook = Console.ReadLine();
+                        if (Library.TryAdd(NameOfBook, 0)) Console.WriteLine($"Добавлена книга {NameOfBook}!");
+                        Console.WriteLine();                        
+                        break;
+                    case ConsoleKey.D2:
+                        foreach (var book in Library)
+                        {
+                            Console.WriteLine($"{book.Key} - {book.Value}%;");                           
+                        }
+                        Console.WriteLine();
+                        break;                    
+                    case ConsoleKey.D3:
+                        Console.Clear();
+                        break;
+                    case ConsoleKey.D4:
+                        break;
+                    default:
+                        Console.WriteLine("Неверная команда!");
+                        break;
+
+                }
+            }
+            while (key.Key != ConsoleKey.D4);
+           
         }
     }
 }
