9707ab7b3b2c7dff303b025aca37c81fe406a6a0
DESKTOP-48PS9TT\Макс <maxfire.95@mail.ru>
2022-12-28 21:15:06 +0300

    Выполнил третью часть ДЗ12. ДЗ выполнено полностью.

---
+++ HW_12/HW_12_Jack/Parts.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections.Immutable;
+
+namespace HW_12_Jack
+{
+
+    public class Part1
+    {
+        public ImmutableList<string> Poem { get; set; }
+        public void AddPart(ImmutableList<string> part)
+        {
+            Poem = part.AddRange(new List<string> { "Вот дом,", "Который построил Джек.\n" });
+        }
+    }
+    public class Part2
+    {
+        public ImmutableList<string> Poem { get; set; }
+        public void AddPart(ImmutableList<string> part)
+        {
+
+            Poem = part.AddRange(new List<string> { "А это пшеница,", "Которая в темном чулане хранится",
+                                                           "В доме,", "Который построил Джек.\n"});
+
+        }
+    }
+    public class Part3
+    {
+        public ImmutableList<string> Poem { get; set; }
+        public void AddPart(ImmutableList<string> part)
+        {
+            Poem = part.AddRange(new List<string> { "А это веселая птица-синица,", "Которая часто ворует пшеницу,", "Которая в темном чулане хранится",
+                                                           "В доме,", "Который построил Джек.\n"});
+            
+        }
+    }
+    public class Part4
+    {
+        public ImmutableList<string> Poem { get; set; }
+        public void AddPart(ImmutableList<string> part)
+        {
+            Poem = part.AddRange(new List<string> { "Вот кот,", "Который пугает и ловит синицу,", "Которая часто ворует пшеницу,", "Которая в темном чулане хранится",
+                                                           "В доме,", "Который построил Джек.\n"});
+
+        }
+    }
+    public class Part5
+    {
+        public ImmutableList<string> Poem { get; set; }
+        public void AddPart(ImmutableList<string> part)
+        {
+            Poem = part.AddRange(new List<string> { "Вот пес без хвоста,", "Который за шиворот треплет кота,", "Который пугает и ловит синицу,",
+                                                        "Которая часто ворует пшеницу,", "Которая в темном чулане хранится",
+                                                           "В доме,", "Который построил Джек.\n"});
+
+        }
+    }
+    public class Part6
+    {
+        public ImmutableList<string> Poem { get; set; }
+        public void AddPart(ImmutableList<string> part)
+        {
+            Poem = part.AddRange(new List<string> { "А это корова безрогая,", "Лягнувшая старого пса без хвоста,", "Который за шиворот треплет кота,",
+                                                            "Который пугает и ловит синицу,", "Которая часто ворует пшеницу,", "Которая в темном чулане хранится",
+                                                           "В доме,", "Который построил Джек.\n"});
+
+        }
+    }
+    public class Part7
+    {
+        public ImmutableList<string> Poem { get; set; }
+        public void AddPart(ImmutableList<string> part)
+        {
+            Poem = part.AddRange(new List<string> { "А это старушка, седая и строгая,", "Которая доит корову безрогую,", "Лягнувшая старого пса без хвоста,",
+                                                    "Который за шиворот треплет кота,", "Который пугает и ловит синицу,", "Которая часто ворует пшеницу,", 
+                                                        "Которая в темном чулане хранится", "В доме,", "Который построил Джек.\n"});
+
+        }
+    }
+    public class Part8
+    {
+        public ImmutableList<string> Poem { get; set; }
+        public void AddPart(ImmutableList<string> part)
+        {
+            Poem = part.AddRange(new List<string> { "А это ленивый и толстый пастух,", "Который бранится с коровницей строгою,", "Которая доит корову безрогую,",
+                                                    "Лягнувшая старого пса без хвоста,", "Который за шиворот треплет кота,", "Который пугает и ловит синицу,", 
+                                                       "Которая часто ворует пшеницу,", "Которая в темном чулане хранится",
+                                                           "В доме,", "Который построил Джек.\n"});
+
+        }
+    }
+    public class Part9
+    {
+        public ImmutableList<string> Poem { get; set; }
+        public void AddPart(ImmutableList<string> part)
+        {
+            Poem = part.AddRange(new List<string> { "Вот два петуха,", "Которые будят того пастуха,", "Который бранится с коровницей строгою,", "Которая доит корову безрогую,",
+                                                    "Лягнувшая старого пса без хвоста,", "Который за шиворот треплет кота,", "Который пугает и ловит синицу,",
+                                                       "Которая часто ворует пшеницу,", "Которая в темном чулане хранится",
+                                                           "В доме,", "Который построил Джек."});
+
+        }
+    }
+}
+
+
--- HW_12/HW_12_Jack/Program.cs
+++ HW_12/HW_12_Jack/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace HW_12_Jack
+﻿using System.Collections.Immutable;
+
+namespace HW_12_Jack
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           
+            ImmutableList<string> Empty = ImmutableList.Create<string>();
+            Part1 MyPart1 = new Part1();
+            Part2 MyPart2 = new Part2();
+            Part3 MyPart3 = new Part3();
+            Part4 MyPart4 = new Part4();
+            Part5 MyPart5 = new Part5();
+            Part6 MyPart6 = new Part6();
+            Part7 MyPart7 = new Part7();
+            Part8 MyPart8 = new Part8();
+            Part9 MyPart9 = new Part9();
+            MyPart1.AddPart(Empty);
+            MyPart2.AddPart(MyPart1.Poem);
+            MyPart3.AddPart(MyPart2.Poem);
+            MyPart4.AddPart(MyPart3.Poem);
+            MyPart5.AddPart(MyPart4.Poem);
+            MyPart6.AddPart(MyPart5.Poem);
+            MyPart7.AddPart(MyPart6.Poem);
+            MyPart8.AddPart(MyPart7.Poem);
+            MyPart9.AddPart(MyPart8.Poem);
+            Console.WriteLine(string.Join("\n", MyPart9.Poem));
+
+            Console.WriteLine();
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write($"Количество элементов в первоначальной коллекции: {Empty.Count}");
+            Console.ResetColor();
         }
     }
 }
