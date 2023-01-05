a858a8a3ccde0701988695196029b7b22320bcdc
DESKTOP-48PS9TT\Макс <maxfire.95@mail.ru>
2023-01-05 17:39:16 +0300

    ДЗ №13 выполнено

---
+++ HW_13/HW_13/IEnumerableExtention.cs
@@ -0,0 +1,30 @@
+﻿namespace HW_13
+{
+    public static class IEnumerableExtention
+    {
+        public static IEnumerable<T> Top<T>(this IEnumerable<T> collection, int percent)
+        {
+            if ((percent < 0) || (percent > 100)) throw new ArgumentException();
+            int countToExtract = (int)Math.Round((double)collection.Count() * percent / 100, MidpointRounding.ToPositiveInfinity);
+            //вариант с отложенным выполнением:
+            for (int i = collection.Count() - 1; i >= (collection.Count() - countToExtract); i--)
+            {
+                yield return collection.ElementAt<T>(i);
+            }
+            //реализовал также вариант в более коротком виде, но с немедленным выполнением:
+            //return collection.Take((collection.Count() - countToExtract)..collection.Count()).Reverse();
+            //оба метода работают, но для них нужен разный способ отлавливания исключений в main
+        }
+        public static IEnumerable<T> Top<T>(this IEnumerable<T> collection, int percent, Func<T, int> field)
+        {
+            if ((percent < 0) || (percent > 100)) throw new ArgumentException();
+
+            var sortCollection = collection.OrderBy(field);
+            int countToExtract = (int)Math.Round((double)collection.Count() * percent / 100, MidpointRounding.ToPositiveInfinity);
+            for (int i = collection.Count() - 1; i >= (collection.Count() - countToExtract); i--)
+            {
+                yield return sortCollection.ElementAt<T>(i);
+            }
+        }
+    }
+}
---
+++ HW_13/HW_13/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace HW_13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            
+                var result = list.Top(30);
+                var resultEr = list.Top(101);
+            try
+            {
+                foreach (var item in result)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            catch (ArgumentException) { Console.WriteLine("Неверный аргумент!"); }
+            try
+            {
+                foreach (var item in resultEr)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            catch (ArgumentException) { Console.WriteLine("Неверный аргумент!"); }
+            var people = new List<Person> { 
+                new Person(12, "Вася"),
+                new Person(15, "Лена"),
+                new Person(45, "Федор"),
+                new Person(22, "Анна"),
+                new Person(13, "Машенька"),
+                new Person(95, "Василий")
+                };
+            var result2 = people.Top(60, Person=>Person.Age);
+            var resultEr2 = people.Top(101, Person => Person.Age);
+            try
+            {
+                foreach (var item in result2)
+                {
+                    Console.WriteLine($"{item.Name} - {item.Age} лет/года");
+                }
+            }
+            catch (ArgumentException) { Console.WriteLine("Неверный аргумент!"); }
+            try
+            {
+                foreach (var item in resultEr2)
+                {
+                    Console.WriteLine($"{item.Name} - {item.Age} лет/года");
+                }
+            }
+            catch (ArgumentException) { Console.WriteLine("Неверный аргумент!"); }
+
+        }
+        public class Person
+        {
+            public int Age { get; }
+            public string Name { get; }
+            public Person(int age, string name)
+            {
+                Age = age;
+                Name= name;
+            }
+        }
+    }
+}
