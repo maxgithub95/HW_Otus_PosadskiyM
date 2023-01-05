8fd7aa7bd71a2d00550b3eb710ae5a437096bdb4
DESKTOP-48PS9TT\Макс <maxfire.95@mail.ru>
2023-01-05 19:24:47 +0300

    Ввел правки в ДЗ №13

--- HW_13/HW_13/IEnumerableExtention.cs
+++ HW_13/HW_13/IEnumerableExtention.cs
@@ -5,26 +5,24 @@
         public static IEnumerable<T> Top<T>(this IEnumerable<T> collection, int percent)
         {
             if ((percent < 0) || (percent > 100)) throw new ArgumentException();
+            var sortCollection = collection.OrderByDescending(collection=>collection);
             int countToExtract = (int)Math.Round((double)collection.Count() * percent / 100, MidpointRounding.ToPositiveInfinity);
             //вариант с отложенным выполнением:
-            for (int i = collection.Count() - 1; i >= (collection.Count() - countToExtract); i--)
-            {
-                yield return collection.ElementAt<T>(i);
-            }
-            //реализовал также вариант в более коротком виде, но с немедленным выполнением:
-            //return collection.Take((collection.Count() - countToExtract)..collection.Count()).Reverse();
+            //for (int i = 0; i < countToExtract; i++)
+            //{
+            //    yield return sortCollection.ElementAt<T>(i);
+            //}
+            //реализовал также вариант в более коротком виде, но с немедленным выполнением, хотя в описании метода написано, что он также с отложенным выполнением :
+            return sortCollection.Take(countToExtract);
             //оба метода работают, но для них нужен разный способ отлавливания исключений в main
         }
         public static IEnumerable<T> Top<T>(this IEnumerable<T> collection, int percent, Func<T, int> field)
         {
             if ((percent < 0) || (percent > 100)) throw new ArgumentException();
 
-            var sortCollection = collection.OrderBy(field);
+            var sortCollection = collection.OrderByDescending(field);
             int countToExtract = (int)Math.Round((double)collection.Count() * percent / 100, MidpointRounding.ToPositiveInfinity);
-            for (int i = collection.Count() - 1; i >= (collection.Count() - countToExtract); i--)
-            {
-                yield return sortCollection.ElementAt<T>(i);
-            }
+            return sortCollection.Take(countToExtract);
         }
     }
 }
--- HW_13/HW_13/Program.cs
+++ HW_13/HW_13/Program.cs
@@ -1,17 +1,14 @@
-﻿using System.Security.Cryptography.X509Certificates;
-
-namespace HW_13
+﻿namespace HW_13
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             var list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-            
-                var result = list.Top(30);
-                var resultEr = list.Top(101);
+
             try
             {
+                var result = list.Top(30);
                 foreach (var item in result)
                 {
                     Console.WriteLine(item);
@@ -20,13 +17,15 @@ namespace HW_13
             catch (ArgumentException) { Console.WriteLine("Неверный аргумент!"); }
             try
             {
+                var resultEr = list.Top(101);
                 foreach (var item in resultEr)
                 {
                     Console.WriteLine(item);
                 }
             }
             catch (ArgumentException) { Console.WriteLine("Неверный аргумент!"); }
-            var people = new List<Person> { 
+                       
+            var people = new List<Person> {
                 new Person(12, "Вася"),
                 new Person(15, "Лена"),
                 new Person(45, "Федор"),
@@ -34,10 +33,9 @@ namespace HW_13
                 new Person(13, "Машенька"),
                 new Person(95, "Василий")
                 };
-            var result2 = people.Top(60, Person=>Person.Age);
-            var resultEr2 = people.Top(101, Person => Person.Age);
             try
             {
+                var result2 = people.Top(70, Person => Person.Age);
                 foreach (var item in result2)
                 {
                     Console.WriteLine($"{item.Name} - {item.Age} лет/года");
@@ -46,6 +44,8 @@ namespace HW_13
             catch (ArgumentException) { Console.WriteLine("Неверный аргумент!"); }
             try
             {
+                var resultEr2 = people.Top(10, Person => Person.Age);
+            
                 foreach (var item in resultEr2)
                 {
                     Console.WriteLine($"{item.Name} - {item.Age} лет/года");
@@ -61,7 +61,7 @@ namespace HW_13
             public Person(int age, string name)
             {
                 Age = age;
-                Name= name;
+                Name = name;
             }
         }
     }
