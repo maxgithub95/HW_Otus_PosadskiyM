99eb873b887c8fcb7b3ae9d4a5e385db1a1434b2
DESKTOP-48PS9TT\Макс <maxfire.95@mail.ru>
2023-01-07 19:15:05 +0300

    убрал лишнее

--- HW_13/HW_13/IEnumerableExtention.cs
+++ HW_13/HW_13/IEnumerableExtention.cs
@@ -5,16 +5,9 @@
         public static IEnumerable<T> Top<T>(this IEnumerable<T> collection, int percent)
         {
             if ((percent < 0) || (percent > 100)) throw new ArgumentException();
-            var sortCollection = collection.OrderByDescending(collection=>collection);
+            var sortCollection = collection.OrderByDescending(collection => collection);
             int countToExtract = (int)Math.Round((double)collection.Count() * percent / 100, MidpointRounding.ToPositiveInfinity);
-            //вариант с отложенным выполнением:
-            //for (int i = 0; i < countToExtract; i++)
-            //{
-            //    yield return sortCollection.ElementAt<T>(i);
-            //}
-            //реализовал также вариант в более коротком виде, но с немедленным выполнением, хотя в описании метода написано, что он также с отложенным выполнением :
             return sortCollection.Take(countToExtract);
-            //оба метода работают, но для них нужен разный способ отлавливания исключений в main
         }
         public static IEnumerable<T> Top<T>(this IEnumerable<T> collection, int percent, Func<T, int> field)
         {
